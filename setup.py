d783148565301597488547113fa2c576f010edbd
Wellington Gadelha <wellingtongadelha@Informeai.local>
2020-02-05 19:51:19 -0300

    Primeiro commit do Projeto.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="visionPDF", # Nome do Pacote
+    version="1.0.1",
+    author="Wellington Gadelha",
+    author_email="contato.informeai@gmail.com",
+    description="Módulo que ler,traduz e escreve arquivos PDF.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/informeai/vision",
+    packages=setuptools.find_packages(),
+    install_requires=['PyPDF2','reportlab','karkipytranslator'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
